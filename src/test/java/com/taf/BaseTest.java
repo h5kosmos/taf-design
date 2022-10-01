77ccb9005b73ef2993a8e9935e0841f05136872c
Denis Avdeev <denis_avdeev@epam.com>
2022-10-01 18:48:56 +0400

    add ci

--- src/test/java/com/taf/BaseTest.java
+++ src/test/java/com/taf/BaseTest.java
@@ -5,11 +5,6 @@ import org.testng.annotations.Test;
 
 public class BaseTest {
 
-    @Test(description = "Failed test")
-    public void failedTest() {
-        Assert.assertTrue(false);
-    }
-
     @Test(description = "Success test")
     public void successTest() {
 
