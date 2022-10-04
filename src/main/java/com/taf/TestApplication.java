ec2953b40249fdfbd420c6e078ae06f88bbe6e6e
Denis Avdeev <denis_avdeev@epam.com>
2022-10-04 22:47:34 +0400

    Add spring boot and config property reader

---
+++ src/main/java/com/taf/TestApplication.java
@@ -0,0 +1,15 @@
+package com.taf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
--- src/test/java/com/taf/BaseTest.java
+++ src/test/java/com/taf/BaseTest.java
@@ -1,12 +1,20 @@
 package com.taf;
 
-import org.testng.Assert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-public class BaseTest {
+@Slf4j
+@SpringBootTest(classes = TestApplication.class)
+public class BaseTest extends AbstractTestNGSpringContextTests {
+
+    @Value("${test.property}")
+    private String testProperty;
 
     @Test(description = "Success test")
     public void successTest() {
-
+        log.info("testProperty: " + testProperty);
     }
 }
