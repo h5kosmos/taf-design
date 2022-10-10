fcb9e03bc77f7546d280f3f28286726bd4b47480
Denis Avdeev <denis_avdeev@epam.com>
2022-10-11 00:07:59 +0400

    replace testNG to junit5

--- src/test/java/com/taf/BaseTest.java
+++ src/test/java/com/taf/BaseTest.java
@@ -2,42 +2,44 @@ package com.taf;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.testng.TextReport;
 import com.taf.core.DriverManager;
 import lombok.extern.slf4j.Slf4j;
 
-import org.openqa.selenium.WebDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.testng.annotations.*;
+
 
 import static com.codeborne.selenide.Selenide.open;
 
 @Slf4j
 @WebAppConfiguration
 @SpringBootTest(classes = TestApplication.class)
-@Listeners({TextReport.class})
-public class BaseTest extends AbstractTestNGSpringContextTests {
+//@Listeners({TextReport.class})
+public class BaseTest {
     @Autowired
     private DriverManager driverManager;
 
     @Value("${test.property}")
     private String testProperty;
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeEach
     public void setUp() {
         WebDriverRunner.setWebDriver(driverManager.initDriver());
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterEach
     public void tearDown() {
         Selenide.closeWebDriver();
     }
 
-    @Test(description = "Success test")
+    @Test
+    @DisplayName("Success test")
     public void successTest() {
         open("https://www.duckduckgo.com");
         log.info("testProperty: " + testProperty);
