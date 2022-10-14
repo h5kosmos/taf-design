29e6bb0f6c8d3fc77c0eb0f5b3df99d545492fe5
Denis Avdeev <denis_avdeev@epam.com>
2022-10-14 15:53:52 +0400

    fix gradle parallel

--- src/test/java/com/taf/CucumberTest.java
+++ src/test/java/com/taf/CucumberTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @RunWith(Cucumber.class)
 @SpringBootTest(classes = TestApplication.class)
 @CucumberOptions(
-        features = "src/test/resources/Feature/Launches_Test",
+        features = "src/test/resources/Feature",
         glue = {"com.taf.steps", "com.taf.configurations"}
 )
 public class CucumberTest {
